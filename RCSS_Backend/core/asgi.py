e03309da7214752e5b643f21862104069630d2dd
User <kelly850408@gmail.com>
2024-10-07 15:36:29 +0800

    Change asgi setting # Please enter the commit message for your changes. Lines starting

--- RCSS_Backend/core/asgi.py
+++ RCSS_Backend/core/asgi.py
@@ -10,7 +10,20 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from django.urls import path
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.consumers import ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            [
+                path('ws/chat/', ChatConsumer.as_asgi()),
+            ]
+        )
+    ),
+})
