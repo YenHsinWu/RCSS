473eaff7022cca1612650ed5d157391948cecadc
LAPTOP-BJBNG77F\user <michaelhu1106@gmail.com>
2024-11-26 12:13:51 +0800

    客服聊天室管理-完成

---
+++ SignalRChat/SignalRChat/SignalRChat.Client/Model/BusinessServiceList.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRChat.Client.Model
+{
+    public class BusinessServiceList
+    {
+        public DataBusinessServiceList[]? data { get; set; }
+        public int dataBusinessServiceListCount { get; set; }
+        public string code { get; set; }
+        public string message { get; set; }
+    }
+}
--- SignalRChat/SignalRChat/SignalRChat.Client/Service/BusinessService.cs
+++ SignalRChat/SignalRChat/SignalRChat.Client/Service/BusinessService.cs
@@ -105,8 +105,43 @@ namespace SignalRChat.Client.Service
             {
                 dataBusinessList = null,
                 dataBusinessType = null,
-                code = "10",
-                message = "$\"錯誤: {err}"
+                code = "-1",
+                message = $"錯誤: {err}"
+            };
+        }
+        public async Task<SignalRChat.Client.Model.BusinessServiceList> GetBusinessServiceList(string business_service_name, int business_id, int count_per_page, int page)
+        {
+            string err = "";
+            try
+            {
+                var basePath = $"{_configuration["BaseUri"]}businessServiceList";  // "http://10.10.10.207:3000/api/businessList";
+                var uri = ParameterHelper.BuildUrlWithQueryStringUsingStringConcat(basePath, new Dictionary<string, string>
+                {
+                    { "page",page.ToString()},
+                    { "count_per_page",count_per_page.ToString()},
+                    { "business_id",business_id.ToString()},
+                    { "business_service_name",business_service_name}
+                }
+                );
+                var response = await _httpClient.GetAsync(uri);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    SignalRChat.Client.Model.BusinessServiceList jsonData = JsonSerializer.Deserialize<SignalRChat.Client.Model.BusinessServiceList>(data);
+                    return jsonData;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"錯誤: {ex.Message}");
+                err = ex.Message;
+            }
+            return new SignalRChat.Client.Model.BusinessServiceList
+            {
+                data = null,
+                code = "-1",
+                message = $"錯誤: {err}"
             };
         }
         public async Task<List<DataBusinessType>?> GetBusinessType()
@@ -171,7 +206,42 @@ namespace SignalRChat.Client.Service
             return new ResponseStanderd
             {
                 code = "-1",
-                message = "$\"錯誤: {err}"
+                message = $"錯誤: {err}"
+            };
+        }
+        public async Task<ResponseStanderd> PostBusinessServiceList(int business_id, string business_service_name, int backend_user_id)
+        {
+            string err = "";
+            try
+            {
+                var basePath = $"{_configuration["BaseUri"]}businessservicelist";  // "http://10.10.10.207:3000/api/businessList";
+                var uri = basePath;
+                using StringContent jsonContent = new(
+                    JsonSerializer.Serialize(new
+                    {
+                        business_id = business_id,
+                        business_service_name = business_service_name,
+                        backend_user_id = backend_user_id
+                    }),
+                    Encoding.UTF8,
+                    "application/json");
+                var response = await _httpClient.PostAsync(uri, jsonContent);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    ResponseStanderd jsonData = JsonSerializer.Deserialize<ResponseStanderd>(data);
+                    return jsonData;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"錯誤: {ex.Message}");
+                err = ex.Message;
+            }
+            return new ResponseStanderd
+            {
+                code = "-1",
+                message = $"錯誤: {err}"
             };
         }
         public async Task<UpdateBusinessList> GetUpdateBusinessList(int business_id)
@@ -200,7 +270,7 @@ namespace SignalRChat.Client.Service
                 dataBusinessList = null,
                 dataBusinessType = null,
                 code = "10",
-                message = "$\"錯誤: {err}"
+                message = $"錯誤: {err}"
             };
         }
         public async Task<ResponseStanderd> PutBusinessList(int business_id, string address, string phone, string email, string business_name, int business_type_id, int backend_user_id, string business_url)
@@ -240,7 +310,7 @@ namespace SignalRChat.Client.Service
             return new ResponseStanderd
             {
                 code = "-1",
-                message = "$\"錯誤: {err}"
+                message = $"錯誤: {err}"
             };
         }
 
@@ -273,7 +343,39 @@ namespace SignalRChat.Client.Service
             return new ResponseStanderd
             {
                 code = "-1",
-                message = "$\"錯誤: {err}"
+                message = $"錯誤: {err}"
+            };
+        }
+        public async Task<ResponseStanderd> DeleteBusinessServiceList(int business_id, string business_service_name)
+        {
+            string err = "";
+            try
+            {
+                var basePath = $"{_configuration["BaseUri"]}businessservicelist/{business_id}/{business_service_name}";  // "http://10.10.10.207:3000/api/businessList";
+                var uri = basePath;
+                //using StringContent jsonContent = new(
+                //    JsonSerializer.Serialize(new {                        
+                //        business_id = business_id
+                //    }),
+                //    Encoding.UTF8,
+                //    "application/json");
+                var response = await _httpClient.DeleteAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    ResponseStanderd jsonData = JsonSerializer.Deserialize<ResponseStanderd>(data);
+                    return jsonData;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"錯誤: {ex.Message}");
+                err = ex.Message;
+            }
+            return new ResponseStanderd
+            {
+                code = "-1",
+                message = $"錯誤: {err}"
             };
         }
     }
