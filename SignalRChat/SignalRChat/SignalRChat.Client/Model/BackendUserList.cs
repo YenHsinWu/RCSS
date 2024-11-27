2d398e3afe5b11d4fdfc92f542cb7d6f2f9ea5aa
LAPTOP-BJBNG77F\user <michaelhu1106@gmail.com>
2024-11-27 15:18:12 +0800

    後台用戶管理-新增完成

---
+++ SignalRChat/SignalRChat/SignalRChat.Client/Model/BackendUserList.cs
@@ -0,0 +1,10 @@
+﻿namespace SignalRChat.Client.Model
+{
+    public class BackendUserList
+    {
+        public DataBackendUserList[]? data { get; set; }
+        public int dataBackendUsersCount { get; set; } = default;
+        public string code { get; set; }
+        public string message { get; set; }
+    }
+}
